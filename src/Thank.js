9e5ddac2159d3f1bb279b69afa63b8d345e9c231
calculito <ion.oaie@gmail.com>
2020-12-24 11:48:12 +0100

    thanks

--- src/Thank.js
+++ src/Thank.js
@@ -1,31 +1,58 @@
 import React, { useState } from "react";
 import Timer from "./Timer";
-import Piano from "./Piano";
+import Swal from "sweetalert2";
+
 export default function Thank({ onGridClick }) {
-  const [thank, setthank] = useState(undefined);
+  const allthanks = [
+    "Thank you for all your hard work on this",
+    "Thanks again, we couldn’t have pulled this off without you",
+    "Thank you, you’re amazing!",
+    "I’m so thankful for everything you bring to the table",
+    "Thank you kindly",
+    "Warmest thanks",
+    "I truly appreciate your hard work",
+    "From the bottom of my heart, thank you",
+    "Please accept my deepest thanks",
+    "I can’t thank you enough",
+    "Grateful for your support",
+    "I appreciate your thoughtfulness, you’ve made my day!",
+    "Thank you for going above and beyond!",
+    "Thanks for being in my corner",
+    "So glad you’re on my team",
+    "I appreciate your assistance and look forward to your continuing to work on our account.",
+    "Thanks very much for the assistance you provide my business. It is sincerely appreciated.",
+  ];
+  const thankschosen = (i) => {
+    console.log(i);
+    Swal.fire({
+      title: "Nice!",
+      text: "Now open your messaging app, paste it there and send it!",
+      icon: "success",
+      confirmButtonText: "OK",
+    });
+    navigator.clipboard.writeText(allthanks[i]);
+  };
   return (
     <div className="containerColumn">
-      <Piano />
       <Timer onGridClick={onGridClick} />
-      <form className="form">
-        <textarea
-          className="bigWindowForText"
-          autoFocus
-          type="text"
-          placeholder={
-            thank === undefined
-              ? "That is just a placeholder. Type your text to say thank you to someone. Gratitude is a way for people to appreciate what they have instead of always reaching for something new in the hopes it will make them happier, or thinking they can't feel satisfied until every physical and material need is met."
-              : undefined
-          }
-          value={thank}
-          onChange={(e) => setthank(e.target.value)}
-          required
-        />
-      </form>
-      <div className="minitext">
-        <a href="https://www.health.harvard.edu/healthbeat/giving-thanks-can-make-you-happier#:~:text=In%20positive%20psychology%20research%2C%20gratitude,express%20gratitude%20in%20multiple%20ways.">
-          see here more...
-        </a>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div style={{ textAlign: "center" }}>
+        <span>Click on your favourite text to copy it to the clipboard.</span>
+      </div>
+      <div className="containergrates">
+        {allthanks.map((data, i) => {
+          return (
+            <div
+              key={"grates" + i}
+              className={"papernotesreihen"}
+              onClick={() => thankschosen(i)}
+            >
+              {data}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
